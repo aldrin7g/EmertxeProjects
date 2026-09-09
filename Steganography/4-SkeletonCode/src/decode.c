e2374b5e8eed998376bd920dce0fa9f90c741bb6
aldrin7g <aldrin7g@gmail.com>
2026-09-09 11:29:55 +0530

    decode print info simplified

--- Steganography/4-SkeletonCode/src/decode.c
+++ Steganography/4-SkeletonCode/src/decode.c
@@ -3,7 +3,6 @@
 
 Status read_and_validate_decode_args(char* argv[], DecodeInfo* decInfo){
     printf(O "## Decoding Procedure Started ##\n" Rst);
-    printf(I "INFO: Validating Decode Arguments\n" Rst);
     // Get the source image file name and extn
     if(!get_extn(decInfo->extn_stego_file, argv[2])){
         fprintf(stderr,E "Error: Invalid Source file Name \"%s\"!\n" Rst, argv[2]);
@@ -20,7 +19,7 @@ Status read_and_validate_decode_args(char* argv[], DecodeInfo* decInfo){
         printf(I "INFO: Output filename not mentioned, Creating \"decoded.txt\" as default\n" Rst);
         strcpy(decInfo->secret_fname, "decoded.txt");
     }
-    printf(S "INFO: Done\n" Rst);
+    printf(I "INFO: Decode Arguments Validation Successful\n" Rst);
     return success;
 }
 
@@ -37,9 +36,6 @@ Status open_stego_file(DecodeInfo* decInfo){
     }
     free(output_path);
     printf(I "INFO: Opened Stego Image File: %s\n" Rst, decInfo->stego_image_fname);
-
-    // No failure return success
-    printf(S "INFO: Done\n" Rst);
     return success;
 }
 
@@ -56,9 +52,6 @@ Status open_secret_file(DecodeInfo* decInfo){
     }
     free(output_path);
     printf(I "INFO: Opened Secret File: %s\n" Rst, decInfo->secret_fname);
-
-    // No failure return success
-    printf(S "INFO: Done\n" Rst);
     return success;
 }
 
@@ -95,7 +88,7 @@ Status check_magic_string(DecodeInfo* decInfo){
     magic[MAGIC_STRING_LEN] = '\0';
 
     if(!strcmp(magic, MAGIC_STRING)){
-        printf(S "INFO: Done. Magic String Matched!\n" Rst);
+        printf(I "INFO: Magic String Matched!\n" Rst);
         return success;
     }
     else{
@@ -106,24 +99,20 @@ Status check_magic_string(DecodeInfo* decInfo){
 
 Status decode_secret_file_extn(DecodeInfo* decInfo){
     // Decode the size of the secret file extension
-
-    printf(I "INFO: Decoding Size of Secret File Extension\n" Rst);
     char extn_size;
     if(!decode_data_from_image(&extn_size, 1, decInfo->fptr_stego_image)){
         fprintf(stderr, E "ERROR: Decoding Secret File Extension Size failed\n" Rst);
         return failure;
     }
-    printf(S "INFO: Done\n" Rst);
-
+    printf(I "INFO: Decoded Secret File Extension Size: %d bytes\n" Rst, extn_size);
     char extn[extn_size+1];
     extn[extn_size] = '\0';
     // Decode the secret file extension
-    printf(I "INFO: Decoding Secret File Extension\n" Rst);
     if(!decode_data_from_image(extn, extn_size, decInfo->fptr_stego_image)){
         fprintf(stderr, E "ERROR: Decoding Secret File Extension failed\n" Rst);
         return failure;
     }
-    printf("%s\n%s\n", extn, decInfo->extn_secret_file);
+    printf(I "INFO: Decoded Secret File Extension: \"%s\"\n" Rst, extn);
     if(strcmp(extn, decInfo->extn_secret_file)){
         strcpy(decInfo->extn_secret_file, extn);
         char* dot = strchr(decInfo->secret_fname, '.');
@@ -131,32 +120,28 @@ Status decode_secret_file_extn(DecodeInfo* decInfo){
         strcat(decInfo->secret_fname, decInfo->extn_secret_file);
     }
     open_secret_file(decInfo);
-    printf("%s\n", decInfo->secret_fname);
-    printf(S "INFO: Done\n" Rst);
     return success;
 }
 
 Status decode_secret_file_size(DecodeInfo* decInfo){
-    printf(I "INFO: Decoding Secret Data Size\n" Rst);
     char size[sizeof(int)];
     if(!decode_data_from_image(size, sizeof(int), decInfo->fptr_stego_image)){
         fprintf(stderr, E "ERROR: Decoding Secret File Size failed\n" Rst);
         return failure;
     }
     memcpy(&decInfo->size_secret_file, size, sizeof(int));
-    printf(S "INFO: Done\n" Rst);
+    printf(I "INFO: Decoded Secret Data Size: %d bytes\n" Rst, decInfo->size_secret_file);
     return success;
 }
 
 Status decode_secret_file_data(DecodeInfo* decInfo){
-    printf(I "INFO: Decoding Secret File Data\n" Rst);
     char data[decInfo->size_secret_file];
     if(!decode_data_from_image(data, decInfo->size_secret_file, decInfo->fptr_stego_image)){
-        fprintf(stderr, E "ERROR: Decoding Secret File Data failed\n" Rst);
+        fprintf(stderr, E "ERROR: Decoding Secret File Data failed😟\n" Rst);
         return failure;
     }
     fwrite(data, sizeof(char), decInfo->size_secret_file, decInfo->fptr_secret);
-    printf(S "INFO: Done\n" Rst);
+    printf(I "INFO: Decoded Secret File Data Successfully🤩\n" Rst);
     return success;
 }
 
--- Steganography/4-SkeletonCode/src/steganography.c
+++ Steganography/4-SkeletonCode/src/steganography.c
@@ -6,9 +6,7 @@
 int main(int argc, char *argv[])
 {
     OperationType input = check_operation_type(argv);
-
-    printf(I "INFO: Operation Type: %s\n" Rst, input == unsupported ? "<Unknown>" : input == encode ? "Encode" : "Decode");
-
+    //printf(I "INFO: Operation Type: %s\n" Rst, input == unsupported ? "<Unknown>" : input == encode ? "Encode" : "Decode");
     if(input == encode){
         // do encode
         EncodeInfo encInfo;
